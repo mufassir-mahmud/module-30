f1acddc75b911c7a52ab36a52ecd06a3780b8831
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-10-30 01:40:37 +0600

    first commit

---
+++ default.js
@@ -0,0 +1,5 @@
+function add(num1, num2= 20){
+    const result = num1+ num2;
+    console.log(num1, num2, result);
+}
+const sum = add(10)
---
+++ var-let-const.js
@@ -0,0 +1,15 @@
+const money = 50;
+// money = 25;
+const numbers = [11,22,33,44,55,66,77];
+numbers[1] = 23;
+numbers.push(88,99)
+console.log(numbers);
+
+const student = {
+    name: 'Mufassir',
+    age: 21,
+    code: 484121,
+}
+for(let i = 0; i<10; i++){
+    console.log('Mufassir');
+}
